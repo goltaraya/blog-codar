b9499a6d900d8eaaf1239e473269fa7dad7bf0d6
Yago Goltara <goltarayago@gmail.com>
2022-07-11 16:31:45 -0300

    Estilizando os posts

--- post.php
+++ post.php
@@ -1,4 +1,5 @@
 <?php
+    
     include_once("templates/header.php");
 
     if(isset($_GET['id'])){
@@ -18,7 +19,7 @@
     <div class="content-container">
         <h1 id="main-title"><?= $currentPost['title'] ?></h1>
         <p id="post-description"> <?= $currentPost['description'] ?></p>
-        <div>
+        <div class="img-container">
             <img src="<?= $BASE_URL?>img/<?= $currentPost['img'] ?>" alt="<?= $currentPost['title'] ?>">
         </div>
         <p class="post-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, possimus quibusdam ad totam voluptate non consequatur ducimus laboriosam debitis sunt maiores in sit perferendis, quidem illo doloribus officiis ipsa. Quod!
@@ -34,21 +35,21 @@
         Assumenda placeat illo omnis? Et neque praesentium, temporibus doloribus alias dolore, iste tenetur voluptatem inventore deserunt ullam repellendus corrupti reprehenderit aperiam voluptate nihil expedita numquam nisi, totam quos explicabo voluptates.
         </p>
     </div>
-</main>
 
-<aside id="nav-container">
-    <h3 id="tags-title">Tags</h3>
-    <ul id="tag-list">
-        <?php foreach($currentPost['tags'] as $tag): ?>
-            <li><a href="#"><?= $tag ?></a></li>
-        <?php endforeach; ?>
-    </ul>
-    <h3 id="categories-title">Categorias</h3>
-    <ul id="tag-list">
-        <?php foreach($categories as $category): ?>
-            <li><a href="#"><?= $category ?></a></li>
-        <?php endforeach; ?>
-    </ul>
-</aside>
+    <aside id="nav-container">
+        <h3 id="tags-title">Tags</h3>
+        <ul id="tag-list">
+            <?php foreach($currentPost['tags'] as $tag): ?>
+                <li><a href="#"><?= $tag ?></a></li>
+            <?php endforeach; ?>
+        </ul>
+        <h3 id="categories-title">Categorias</h3>
+        <ul id="categories-list">
+            <?php foreach($categories as $category): ?>
+                <li><a href="#"><?= $category ?></a></li>
+            <?php endforeach; ?>
+        </ul>
+    </aside>
+</main>
 
 <?php include_once("templates/footer.php") ?>
